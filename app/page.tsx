f364b0c0402af9246bfed3aaa59ad76cf90cf61d
root <root@Stargazer>
2024-05-26 22:43:46 +0700

    Setting Page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { lusitana, dm } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
-    <main className="relative flex min-h-screen flex-col p-6">
+    <main className="relative flex min-h-screen flex-col overflow-auto">
       {/* Background image */}
       <div className="absolute inset-0 z-0">
         <Image
@@ -25,43 +25,43 @@ export default function Page() {
           className="hidden md:block"
         />
       </div>
-      
+
       {/* Header and content */}
       <div className="relative z-10 flex flex-col items-center justify-center p-6 w-full md:w-3/5 md:px-28 md:py-12">
-        <header className="w-full">
-          <nav className="fixed inset-x-0 top-0 z-20 w-full px-4 py-1 bg-transparent transition duration-700 ease-out">
-            <div className="flex justify-between p-4">
-              <div className="text-[1.5rem] md:text-[2rem] leading-[2.5rem] md:leading-[3rem] text-white">
-                <ul className="flex items-center">
-                  <li>
-                    <Image
-                      src="/logo-noodles.png"
-                      alt="logo"
-                      width={60}
-                      height={60}
-                      className="md:w-20 md:h-20"
-                    />
-                  </li>
-                  <li className={`${dm.className} ml-2 md:ml-4 text-white text-2xl md:text-4xl font-bold bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500 bg-clip-text text-transparent animate-pulse`}>
-                    <strong>Restaurant NoodleLab</strong>
-                  </li>
-                </ul>
-                <div className="flex items-center space-x-4 text-lg font-bold-tracking-tight">
-                  <Link href="/menu">
-                    <button className="px-6 py-2 text-white transition-colors bg-gray-800 rounded-full shadow-lg hover:shadow-xl hover:bg-gray-700 md:text-base">Menu</button>
-                  </Link>
-                </div>
-              </div>
-              <Link
-                href="/dashboard"
-                className={`${dm.className} flex items-center gap-3 rounded-lg bg-gradient-to-r from-gray-800 via-gray-600 to-gray-800 px-4 py-2 text-sm font-medium text-white shadow-md transition-transform duration-300 hover:scale-105 hover:from-gray-700 hover:via-gray-500 hover:to-gray-700 md:px-6 md:py-3 md:text-base`}
-              >
-                <UserGroupIcon className="w-5 h-5 md:w-6 md:h-6 text-yellow-400" />
-                <span>Dashboard Admin</span>
-                <ArrowRightIcon className="w-4 h-4 md:w-5 h-5" />
+        <header className="fixed inset-x-0 top-0 z-20 w-full px-4 py-1 bg-transparent transition duration-700 ease-out">
+          <div className="flex justify-between items-center p-4">
+            <div className="flex items-center text-[1.5rem] md:text-[2rem] leading-[2.5rem] md:leading-[3rem] text-white">
+              <Image
+                src="/logo-noodles.png"
+                alt="logo"
+                width={60}
+                height={60}
+                className="md:w-20 md:h-20"
+              />
+              <span className={`${dm.className} ml-2 md:ml-4 text-white text-2xl md:text-4xl font-bold bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500 bg-clip-text text-transparent animate-pulse`}>
+                <strong>NoodleLab</strong>
+              </span>
+            </div>
+            <div className="flex items-center space-x-4 text-lg font-bold tracking-tight">
+              <Link href="/menu-page">
+                <button className="px-6 py-2 text-white transition-colors bg-transparent rounded-full shadow-lg hover:shadow-xl hover:bg-yellow-700 md:text-base">Menu</button>
+              </Link>
+              <Link href="/menu">
+                <button className="px-6 py-2 text-white transition-colors bg-transparent rounded-full shadow-lg hover:shadow-xl hover:bg-yellow-700 md:text-base">Lokasi</button>
+              </Link>
+              <Link href="/menu">
+                <button className="px-6 py-2 text-white transition-colors bg-transparent rounded-full shadow-lg hover:shadow-xl hover:bg-yellow-700 md:text-base">Tentang Kami</button>
               </Link>
             </div>
-          </nav>
+            <Link
+              href="/dashboard"
+              className={`${dm.className} flex items-center gap-3 rounded-lg bg-gradient-to-r from-gray-800 via-gray-600 to-gray-800 px-4 py-2 text-sm font-medium text-white shadow-md transition-transform duration-300 hover:scale-105 hover:from-gray-700 hover:via-gray-500 hover:to-gray-700 md:px-6 md:py-3 md:text-base`}
+            >
+              <UserGroupIcon className="w-5 h-5 md:w-6 md:h-6 text-yellow-400" />
+              <span>Dashboard Admin</span>
+              <ArrowRightIcon className="w-4 h-4 md:w-5 h-5" />
+            </Link>
+          </div>
         </header>
 
         <div className="mt-20 flex grow flex-col gap-4 items-center text-center md:text-left md:items-start md:flex-row">
@@ -77,7 +77,7 @@ export default function Page() {
       <div className="flex-grow"></div>
 
       {/* Footer with Contact Us and Address */}
-      <footer className="relative z-10 mt-12 flex flex-col items-center justify-center w-full bg-black bg-opacity-50 text-white py-6">
+      <footer className="relative z-10 flex flex-col items-center justify-center w-full bg-black bg-opacity-50 text-white py-6">
         <div className="text-center">
           <p className={`${dm.className} text-base md:text-lg`}>Contact Us</p>
           <p className={`${dm.className} text-sm md:text-base`}>
