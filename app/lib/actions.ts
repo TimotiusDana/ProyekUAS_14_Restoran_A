e175e2c73f9cd823363986ce16a53e45c4ae9f4d
root <root@Stargazer>
2024-05-31 19:51:20 +0700

    Fetching + skeleton

---
+++ app/lib/actions.ts
@@ -0,0 +1,248 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { File } from 'buffer';
+// import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+  });
+
+const piss = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string(),
+  image_url: z.string()
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateReservation = FormSchema.omit({ id: true, date: true });
+const UpdateReservation = FormSchema.omit({ id: true, date: true });
+const date = new Date().toISOString().split('T')[0];
+
+const CreateCustomer = piss.omit({ id: true, date: true });
+const UpdateCustomer = piss.omit({ id: true, date: true });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+ 
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function createReservation(formData: FormData) {
+    const { customerId, amount, status } = CreateReservation.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      });
+
+    const amountInCents = amount * 100;
+
+    try{
+    await sql`
+        INSERT INTO reservations (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Reservation.',
+    };
+  }
+    
+
+    revalidatePath('/dashboard/reservations');
+    redirect('/dashboard/reservations');
+}
+
+export async function updateReservation(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateReservation.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    try{
+    await sql`
+      UPDATE reservations
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
+      WHERE id = ${id}
+    `;
+   
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Reservation.' };
+  }
+    revalidatePath('/dashboard/reservations');
+    redirect('/dashboard/reservations');
+  }
+
+export async function deleteReservation(id: string) {
+  throw new Error('Failed to Delete Reservation');
+
+  try{
+  await sql`DELETE FROM reservation WHERE id = ${id}`;
+  revalidatePath('/dashboard/reservations');
+  return { message: 'Deleted Reservation.'};
+}catch (error) {
+  return { message: 'Database Error: Failed to Delete Reservation.' };
+}
+}
+
+export async function createCustomer(formData: FormData) {
+  const img = formData.get('image');
+  console.log(img);
+
+  let fileName = '';
+  if (img instanceof File) {
+    fileName = '/customers/' + img.name;
+    console.log(fileName);
+  }
+
+  const { name, email, image_url } = CreateCustomer.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      image_url: fileName,
+    });
+
+    await sql`
+      INSERT INTO customers (name, email, image_url)
+      VALUES (${name}, ${email}, ${image_url})
+  `;
+
+  revalidatePath('/dashboard/customers');
+  redirect('/dashboard/customers');
+}
+
+export async function updateCustomer(id: string, formData: FormData) {
+  const img = formData.get('image');
+
+  console.log(img);
+  let fileName = '';
+  if (img instanceof File) {
+    fileName = '/customers/' + img.name;
+    console.log(fileName);
+  }
+
+  const { name, email, image_url } = UpdateCustomer.parse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    image_url: fileName,
+  });
+  
+  console.log (name, email, image_url, id);
+  await sql`
+    UPDATE customers
+    SET name = ${name}, email = ${email}, image_url = ${image_url}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/customers');
+  redirect('/dashboard/customers');
+}
+
+export async function deleteCustomer(id: string) {
+  await sql`DELETE FROM customers WHERE id = ${id}`;
+  revalidatePath('/dashboard/customers');
+}
+
+// export async function authenticate(
+//   prevState: string | undefined,
+//   formData: FormData,
+// ) {
+//   try {
+//     await signIn('credentials', formData);
+//   } catch (error) {
+//     if (error instanceof AuthError) {
+//       switch (error.type) {
+//         case 'CredentialsSignin':
+//           return 'Invalid credentials.';
+//         default:
+//           return 'Something went wrong.';
+//       }
+//     }
+//     throw error;
+//   }
+// }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -23,7 +23,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.price, customers.name, customers.image_url, invoices.id
+      SELECT invoices.price, invoices.tax, invoices.payment_methods, invoices.status, invoices.invoice_date, customers.name, customers.image_url, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoice.date DESC
@@ -31,7 +31,7 @@ export async function fetchLatestInvoices() {
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
-      amount: formatCurrency(invoice.amount),
+      price: formatCurrency(invoice.price),
     }));
     return latestInvoices;
   } catch (error: any) {
@@ -45,8 +45,8 @@ export async function fetchCardData() {
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+         SUM(CASE WHEN status = 'paid' THEN price ELSE 0 END) AS "paid",
+         SUM(CASE WHEN status = 'pending' THEN price ELSE 0 END) AS "pending"
          FROM invoices`;
 
     const data = await Promise.all([
@@ -115,7 +115,7 @@ export async function fetchInvoicesPages(query: string) {
       WHERE
         customers.name ILIKE ${`%${query}%`} OR
         customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
+        invoice.price::text ILIKE ${`%${query}%`} OR
         invoice.date::text ILIKE ${`%${query}%`} OR
         invoice.status ILIKE ${`%${query}%`}
     `;
@@ -134,14 +134,17 @@ export async function fetchInvoiceById(id: string) {
       SELECT
         invoices.id,
         invoices.customer_id,
-        invoices.amount,
-        invoices.status
+        invoices.price,
+        invoices.tax,
+        invoices.payment_methods,
+        invoices.status,
+        invoices.invoice_date
       FROM invoices
       WHERE invoices.id = ${id}`;
 
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
-      amount: invoice.amount / 100,
+      price: invoice.price / 100,
     }));
 
     return invoice[0];
@@ -176,8 +179,8 @@ export async function fetchFilteredCustomers(query: string) {
         customers.email,
         customers.image_url,
         COUNT(invoices.id) AS total_invoices,
-        SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-        SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+        SUM(CASE WHEN invoices.status = 'pending' THEN invoices.price ELSE 0 END) AS total_pending,
+        SUM(CASE WHEN invoices.status = 'paid' THEN invoices.price ELSE 0 END) AS total_paid
       FROM customers
       LEFT JOIN invoices ON customers.id = invoices.customer_id
       WHERE
@@ -206,5 +209,105 @@ export async function getUser(email: string) {
   } catch (error: any) {
     console.error('Failed to fetch user:', error);
     throw new Error(`Failed to fetch user. Reason: ${error.message}`);
+  }
+}
+
+export async function fetchLatestReservations() {
+  try {
+    const data = await sql<LatestInvoiceRaw>`
+      SELECT reservations.address, reservations.price, reservations.special_request, reservations.reservation_date, reservations_email, customers.name, customers.image_url, reservations.id
+      FROM reservations
+      JOIN customers ON reservations.customer_id = customers.id
+      ORDER BY invoice.date DESC
+      LIMIT 5`;
+
+    const latestInvoices = data.rows.map((invoice) => ({
+      ...invoice,
+      price: formatCurrency(invoice.price),
+    }));
+    return latestReservations;
+  } catch (error: any) {
+    console.error('Database Error:', error);
+    throw new Error(`Failed to fetch the latest reservations. Reason: ${error.message}`);
+  }
+}
+
+export async function fetchFilteredReservations(query: string, currentPage: number) {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+  try {
+    const reservations = await sql<ReservationsTable>`
+      SELECT
+        reservation.id,
+        reservation.price,
+        reservation.tax,
+        reservation.payment_methods,
+        reservation.status,
+        reservation.reservation_date,
+        customers.name,
+        customers.image_url
+      FROM reservations
+      JOIN customers ON reservations.customer_id = customers.id
+      WHERE
+        customers.name ILIKE ${`%${query}%`} OR
+        reservation.price::text ILIKE ${`%${query}%`} OR
+        reservation.tax::text ILIKE ${`%${query}%`} OR
+        reservation.payment_methods::text ILIKE ${`%${query}%`} OR
+        reservation.status ILIKE ${`%${query}%`} OR
+        reservation.reservation_date ILIKE ${`%${query}%`}
+      ORDER BY reservation.reservation_date DESC
+      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}`;
+
+    return reservations.rows;
+  } catch (error: any) {
+    console.error('Database Error:', error);
+    throw new Error(`Failed to fetch reservations. Reason: ${error.message}`);
+  }
+}
+
+export async function fetchReservationsPages(query: string) {
+  try {
+    const count = await sql`SELECT COUNT(*)
+      FROM reservations
+      JOIN customers ON reservations.customer_id = customers.id
+      WHERE
+        customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`} OR
+        reservation.price::text ILIKE ${`%${query}%`} OR
+        reservation.date::text ILIKE ${`%${query}%`} OR
+        reservation.status ILIKE ${`%${query}%`}
+    `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error: any) {
+    console.error('Database Error:', error);
+    throw new Error(`Failed to fetch total number of reservations. Reason: ${error.message}`);
+  }
+}
+
+export async function fetchReservationById(id: string) {
+  try {
+    const data = await sql<ReservationForm>`
+      SELECT
+        reservations.id,
+        reservations.customer_id,
+        reservations.price,
+        reservations.tax,
+        reservations.payment_methods,
+        reservations.status,
+        reservations.reservation_date
+      FROM reservations
+      WHERE reservations.id = ${id}`;
+
+    const reservation = data.rows.map((reservation) => ({
+      ...reservation,
+      price: reservation.price / 100,
+    }));
+
+    return reservation[0];
+  } catch (error: any) {
+    console.error('Database Error:', error);
+    throw new Error(`Failed to fetch reservation. Reason: ${error.message}`);
   }
 }
