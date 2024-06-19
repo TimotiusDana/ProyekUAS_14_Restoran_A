e4b71d84a5b9178901451cb7403947ab2acccbde
filan214 <valentinus.filan@gmail.com>
2024-06-20 00:38:30 +0700

    check" merge

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -143,25 +143,19 @@ export type MenuForm = {
   name: string;
   category: 'makanan' | 'minuman' ;
   price: number;
-<<<<<<< HEAD
-}
-
-export type MenuTable = {
-  id: string;
-  name: string;
-  category: 'makanan' | 'minuman';
-  price: number;
 
 }
+
 
 export type MenuField = {
   id: string;
   name: string;
   category: 'makanan' | 'minuman';
+  status: 'pending' | 'paid';  
+  
 }
-=======
-  status: 'pending' | 'paid';
-  }
+
+  
 
   export type MenuTable ={
     id: string;
@@ -171,11 +165,4 @@ export type MenuField = {
   
   }
 
-  export type MenuField ={
-    id: string;
-    name: string;
-    category: string;
-    price: number;
   
-  }
->>>>>>> origin/DanielChristianJap
